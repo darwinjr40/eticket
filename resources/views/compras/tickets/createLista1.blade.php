eae0ea2bbb7fd6e09ba5c1de214a9b087992ec74
Darwin Mamani <darwin_xd10@hotmail.com>
2022-07-05 14:30:46 -0400

    darwin, vista de compra ticket

--- resources/views/compras/tickets/createLista1.blade.php
+++ resources/views/compras/tickets/createLista1.blade.php
@@ -16,25 +16,12 @@
                 </div>
 
                 <div id="contenidoSuperior" class="card-body">
-                    {{-- message --}}
-                    @if (isset($error))
-                        <div class="alert alert-danger">
-                            <strong>{{ $error }}</strong>
-                        </div>
-                    @endif
 
-                    @if (isset($success))
-                        <h1>precio</h1>
-                        <div class="alert alert-dark">
-                            <strong>{{ $success }}</strong>
-                        </div>
-                    @endif
                     {{-- izquierda --}}
                     <section id="izquierda" class="caja">
 
-                        <img class="izquierdaClass"
-                            src="{{$imagenes[0]['path']}}">
-                            {{-- src="https://w7.pngwing.com/pngs/981/736/png-transparent-logo-clinic-family-medicine-physician-family-walk-text-trademark-logo.png"> --}}
+                        <img class="izquierdaClass" src="{{ $imagenes[0]['path'] }}">
+                        {{-- src="https://w7.pngwing.com/pngs/981/736/png-transparent-logo-clinic-family-medicine-physician-family-walk-text-trademark-logo.png"> --}}
 
 
                         <div id="map" class="izquierdaClass"></div>
@@ -44,9 +31,9 @@
                     <aside id="derecha" class="caja">
 
                         <div id="derecha1" class="derechaClass">
-                            <h2 class="font-weight-bold" >{{$evento['titulo']}}</h2>
-                            <h5>Categoria: <strong>{{$evento['categoria']}}</strong></h5>
-                            <h5>Descripcion:  <strong>{{$evento['descripcion']}}</strong></h5>
+                            <h2 class="font-weight-bold">{{ $evento['titulo'] }}</h2>
+                            <h5>Categoria: <strong>{{ $evento['categoria'] }}</strong></h5>
+                            <h5>Descripcion: <strong>{{ $evento['descripcion'] }}</strong></h5>
 
                         </div>
                         {{-- <div class="clearfix"></div> --}}
@@ -58,8 +45,8 @@
                                 {{-- <div class="row row justify-content-center"> --}}
                                 {{-- SELECT UBICACION --}}
                                 <div class="derechaCompra">
-                                    <select name="ubicacion_id" id="select-ubicaciones" class="form-control select-control" autofocus
-                                        required>
+                                    <select name="ubicacion_id" id="select-ubicaciones" class="form-control select-control"
+                                        autofocus required>
                                         <option value="">Seleccionar Ubicacion</option>
                                         @foreach ($ubicaciones as $ubicacion)
                                             <option value="{{ $ubicacion['id'] }}">{{ $ubicacion['nombre'] }}
@@ -76,7 +63,7 @@
 
                                 {{-- SELECT Sector --}}
                                 <div id="id-sectores" class="derechaCompra" hidden>
-                                    <select name="sector_id" id="select-sectores" class="form-control select-control" >
+                                    <select name="sector_id" id="select-sectores" class="form-control select-control">
                                         <option value="">Seleccionar Sector</option>
                                     </select>
                                 </div>
@@ -84,7 +71,8 @@
                                 {{-- SELECT Espacio --}}
                                 <div id="id-espacios" class="derechaCompra" hidden>
                                     <h6>Seleccionar Espacio:</h6>
-                                    <select name="espacio_id[]" id="select-espacios" class="form-control select-control" multiple>
+                                    <select name="espacio_id[]" id="select-espacios" class="form-control select-control"
+                                        multiple>
                                         {{-- <option value="">Seleccionar Espacio</option> --}}
                                     </select>
                                 </div>
@@ -118,7 +106,22 @@
                             </form>
                         </div>
                     </aside>
+
                     <div class="clearfix"></div>
+                    {{-- message --}}
+                    <div>
+                        @if (isset($error))
+                            <div class="alert alert-danger">
+                                <strong>{{ $error }}</strong>
+                            </div>
+                        @endif
+    
+                        @if (isset($success))
+                            <div class="alert alert-dark">
+                                <strong>{{ $success }}</strong>
+                            </div>
+                        @endif
+                    </div>
                 </div>
 
 
@@ -128,101 +131,106 @@
 
                 <br>
                 <br>
+                
 
                 @if (isset($tickets) && count($tickets) > 0)
-                    
-                <div class="row">
-                    <div class="col-9">
-                        <h2 class="font-weight-bold" style="margin-left: 45%">Detalles de la compra</h2>
+
+                    <div class="row">
+                        <div class="col-9">
+                            <h2 class="font-weight-bold" style="margin-left: 45%">Detalles de la compra</h2>
+                        </div>
+                        {{-- Button Pagar ahora --}}
+                        <div class="col-3">
+                            <form action="{{ route('tipoPagos.indexTipoPago') }}" role="form"
+                                enctype="multipart/form-data" style="">
+                                @csrf
+                                <div style="margin-block: 10px">
+                                    <button type="button" class="btn btn-default" data-toggle="modal"
+                                        data-target="#modalTigoMoney">
+                                        <img src="{{ asset('img/tigo-money.png') }}" height="30" width="75" />
+                                    </button>
+                                    <button type="button" class="btn btn-default" data-toggle="modal"
+                                        data-target="#modalTarjeta">
+                                        <img src="{{ asset('img/tarjeta.png') }}" height="30" width="75" />
+                                    </button>
+                                </div>
+                                {{-- oculto --}}
+                                <input type="hidden" name="tickets" value="{{ json_encode($tickets) }}">
+                                <input id="evento" type="hidden" name="evento_id"
+                                    value="{{ isset($ubicaciones) ? $ubicaciones[0]->evento_id : '' }}">
+                            </form>
+                        </div>
                     </div>
-                    {{-- Button Pagar ahora --}}
-                    <div class="col-3">
-                        <form action="{{ route('tipoPagos.indexTipoPago') }}" role="form" enctype="multipart/form-data"
-                            style="">
-                            @csrf
-                            <div style="margin-block: 10px">
-                                <button type="button" class="btn btn-default" data-toggle="modal"
-                                    data-target="#modalTigoMoney">
-                                    <img src="{{ asset('img/tigo-money.png') }}" height="30" width="75" />
-                                </button>
-                                <button type="button" class="btn btn-default" data-toggle="modal"
-                                    data-target="#modalTarjeta">
-                                    <img src="{{ asset('img/tarjeta.png') }}" height="30" width="75" />
-                                </button>
-                            </div>
-                            {{-- oculto --}}
-                            <input type="hidden" name="tickets" value="{{ json_encode($tickets) }}">
-                            <input id="evento" type="hidden" name="evento_id"
-                                value="{{ isset($ubicaciones) ? $ubicaciones[0]->evento_id : '' }}">
-                        </form>
-                    </div>
-                </div>
 
 
 
-                {{-- tabla --}}
-                <div style="margin-top: 2%">
-                    <table class="table table-striped" style="margin: 10px auto; width: 80%; border: 1px solid black;">
-                        <thead>
-                            <tr>
-                                {{-- <th scope="col">Codigo</th> --}}
-                                <th scope="col">Ubicacion</th>
-                                <th scope="col">Fecha</th>
-                                <th scope="col">Sector</th>
-                                <th scope="col">Espacio</th>
-                                <th scope="col">cantidad</th>
-                                <th scope="col">precio</th>
-                                <th scope="col">importe</th>
-                                <th scope="col" width='3%'></th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            @foreach ($tickets as $ticket)
+                    {{-- tabla --}}
+                    <div style="margin-top: 2%">
+                        <table class="table table-striped"
+                            style="margin: 10px auto; width: 80%; border: 1px solid black;">
+                            <thead>
                                 <tr>
-                                    {{-- <td>{{ $ticket['id'] }}</td> --}}
-                                    <td>{{ $ticket['ubicacion'] }}</td>
-                                    <td>{{ $ticket['fecha'] }}</td>
-                                    <td>{{ $ticket['sector'] }}</td>
-                                    <td>{{ $ticket['espacio'] }}</td>
-                                    <td>{{ $ticket['cantidad'] }}</td>
-                                    <td>{{ $ticket['precio'] }}</td>
-                                    <td>{{ $ticket['precio'] * $ticket['cantidad'] }}</td>
-                                    <td>
-                                        <form method="POST" action="{{ route('tickets.destroyEvento', $ticket['id']) }}"
-                                            role="form" enctype="multipart/form-data">
-                                            @csrf
-                                            {{-- oculto --}}
-                                            <input type="hidden" name="tickets" value="{{ json_encode($tickets) }}">
-                                            <input id="evento" type="hidden" name="evento_id"
-                                                value="{{ isset($ubicaciones) ? $ubicaciones[0]->evento_id : '' }}">
-                                            {{-- button --}}
-                                            <button class="btn btn-danger btn-sm"
-                                                onclick="return confirm('¿ESTA SEGURO DE  BORRAR?')" value="Borrar"
-                                                title="Borrar">
-                                                {{-- <i class="fas fa-times"></i> --}}
-                                                <i class="far fa-trash-alt"></i>
-                                            </button>
-                                        </form>
-                                    </td>
+                                    {{-- <th scope="col">Codigo</th> --}}
+                                    <th scope="col">Ubicacion</th>
+                                    <th scope="col">Fecha</th>
+                                    <th scope="col">Sector</th>
+                                    <th scope="col">Espacio</th>
+                                    <th scope="col">cantidad</th>
+                                    <th scope="col">precio</th>
+                                    <th scope="col">importe</th>
+                                    <th scope="col" width='3%'></th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                            </thead>
+
+                            <tbody>
+                                @foreach ($tickets as $ticket)
+                                    <tr>
+                                        {{-- <td>{{ $ticket['id'] }}</td> --}}
+                                        <td>{{ $ticket['ubicacion'] }}</td>
+                                        <td>{{ $ticket['fecha'] }}</td>
+                                        <td>{{ $ticket['sector'] }}</td>
+                                        <td>{{ $ticket['espacio'] }}</td>
+                                        <td>{{ $ticket['cantidad'] }}</td>
+                                        <td>{{ $ticket['precio'] }}</td>
+                                        <td>{{ $ticket['precio'] * $ticket['cantidad'] }}</td>
+                                        <td>
+                                            <form method="POST"
+                                                action="{{ route('tickets.destroyEvento', $ticket['id']) }}"
+                                                role="form" enctype="multipart/form-data">
+                                                @csrf
+                                                {{-- oculto --}}
+                                                <input type="hidden" name="tickets"
+                                                    value="{{ json_encode($tickets) }}">
+                                                <input id="evento" type="hidden" name="evento_id"
+                                                    value="{{ isset($ubicaciones) ? $ubicaciones[0]->evento_id : '' }}">
+                                                {{-- button --}}
+                                                <button class="btn btn-danger btn-sm"
+                                                    onclick="return confirm('¿ESTA SEGURO DE  BORRAR?')" value="Borrar"
+                                                    title="Borrar">
+                                                    {{-- <i class="fas fa-times"></i> --}}
+                                                    <i class="far fa-trash-alt"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
 
             </div>
         </div>
     </div>
 
     @endif
-    
+
     <div class="modal fade" id="modalTigoMoney" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"><img src="{{ asset('img/tigo-money.png') }}" height="35" width="95"/></h5>
+                    <h5 class="modal-title" id="exampleModalLabel"><img src="{{ asset('img/tigo-money.png') }}"
+                            height="35" width="95" /></h5>
                 </div>
                 <div class="modal-body">
 
@@ -277,7 +285,8 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">
-                        <img src="{{ asset('img/tarjeta.png') }}" height="35" width="95" /></h5>
+                        <img src="{{ asset('img/tarjeta.png') }}" height="35" width="95" />
+                    </h5>
                 </div>
                 <div class="modal-body">
 
@@ -324,7 +333,8 @@
                                         <span class="input-group-text">
                                             <i class="fa fa-credit-card"> CVC</i>
                                         </span>
-                                        <input type="number" id="cvc" name="cvc" class="form-control" max="999" min="100">
+                                        <input type="number" id="cvc" name="cvc" class="form-control"
+                                            max="999" min="100">
                                     </div>
                                 </div>
                                 <div class="modal-footer">
