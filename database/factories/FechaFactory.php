542dbcc2b851b2b66eef6022f1f5052b566c5840
Darwin Mamani <darwin_xd10@hotmail.com>
2022-07-12 01:37:40 -0400

    darwin, fectories fecha

--- database/factories/FechaFactory.php
+++ database/factories/FechaFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class FechaFactory extends Factory
 {
     protected $ubicaciones = [1, 2, 3, 4, 5, 6, 7];
-    protected $fechas = ['20-08-2011 23:00:02', '20-08-2012 22:00:02', '20-08-2013 21:00:02', '20-08-2014 20:00:02', '20-08-2015 19:00:02', '20-08-2016 18:00:02', '20-08-2023 17:00:02'];
+    protected $fechas = ['2011-08-17 23:00:02', '2012-08-18 22:00:02', '2013-08-19 21:00:02', '2014-08-20 20:00:02', '2015-08-21 19:00:02', '2016-08-22 18:00:02', '2023-08-23 17:00:02'];
 
     public function definition()
     {
