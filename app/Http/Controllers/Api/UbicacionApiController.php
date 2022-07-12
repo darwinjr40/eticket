afcd782646a6bd4db4ec79ff0aa9e61a856888c7
Darwin Mamani <darwin_xd10@hotmail.com>
2022-07-12 01:24:15 -0400

    darwin probando

--- app/Http/Controllers/Api/UbicacionApiController.php
+++ app/Http/Controllers/Api/UbicacionApiController.php
@@ -12,7 +12,7 @@ class UbicacionApiController extends Controller
 {
     public function __construct(){
         // $this->middleware('auth:api');
-        $this->middleware('auth:sanctum');
+        // $this->middleware('auth:sanctum');
     }
     
     public function index()
